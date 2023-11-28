a5832570d73640bc2f776370b6b3ee8959ec2ed6
Arvin Bonggal <arvin.bonggal@gmail.com>
2023-11-28 23:03:24 +0800

    renamed view method to load added print method import os and time apply sleep from time for print display delay apply clear creen from os

--- lead.py
+++ lead.py
@@ -1,4 +1,9 @@
 import json
+import os
+from time import sleep
+
+# clear interpreter variable
+clear = lambda: os.system('cls')
 
 # File Location
 fileLocation = "D:\Documents\GitRepos\simple-python-lead-monitoring-system\json-file\lead.json"
@@ -10,12 +15,13 @@ class Lead:
         self._phoneNumber = phoneNumber
         self._email = email
 
+    # Adding data to JSON file
     def add(self):
         # Lead entry input
         name = self._name
         phoneNumber = self._phoneNumber
         email = self._email
-        leadContent = self.view()
+        leadContent = self.load()
 
         leadEntry = {"name": name, "phoneNumber": phoneNumber, "email": email}
 
@@ -33,18 +39,36 @@ class Lead:
 
         return "Lead added."
 
-    def view(self):
-            
+    # Loading raw data from JSON file
+    def load(self):
         # Initialize JSON file
         with open(fileLocation, "r") as lead:
 
             # Loads JSON file content
             leadContent = json.load(lead)
 
-            return leadContent
+        return leadContent
+    
+    # Printing formatted JSON file content
+    def print(self):
+        leadContent = self.load()
+        # Lead entry counter
+        objectCount = 0
+
+        # Lead entry header
+        print(f"{'Name':<25}{'Phone Number':^20}{'Email Address':>25}")
+
+        # Format Lead entry
+        for object in leadContent["leads"]:
+            print(f"{object['name']:<25}{object['phoneNumber']:^20}{object['email']:>25}")
+            objectCount += 1
+
+        # Lead entry total count
+        print(f"Total Entry: {objectCount}")
 
 # Welcome Message
 print("Welcome to Simple Python Lead Monitoring System")
+
 
 # User Action Logic
 while True:
@@ -62,19 +86,24 @@ while True:
             
             # Call add method from Lead Instace
             print(lead.add())
-
         elif userInput == 2:
             
             # Initialize Lead Instance
             lead = Lead("name", "phoneNumber", "email")
 
             # Call view method from Lead Instance
-            print(lead.view())
-
+            lead.print()
         elif userInput == 3:
+            print("Exiting program...")
             break
         else:
             print("Invalid input.")
 
     except ValueError:
         print("Invalid input.")
+
+    # Pause for n seconds after printing output
+    sleep(5)
+
+    # Clear screen for windows only
+    clear()
